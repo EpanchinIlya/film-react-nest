2ee36c505cb3a4391ddc48121593b3a6bf793208
Epanchin Ilya <epnchin.ilya@yandex.ru>
2025-05-04 19:11:43 +0500

    clear back

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF = 'origin/main',
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'api-service',
+      script: 'backend/dist/main.js', // путь к собранному файлу
+      cwd: 'backend', // текущая рабочая директория
+    },
+  ],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      key: 'C://Users/Work/.ssh/id_rsa_pm2',
+      'pre-deploy': `scp C://Users/Work/.ssh/id_rsa_pm2 ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend`,
+      'post-deploy': `
+      cd ${DEPLOY_PATH}/current/backend &&
+      npm install &&
+      npm run build &&
+      pm2 reload ecosystem.config.js --only api-service
+    `,
+    },
+  },
+};
