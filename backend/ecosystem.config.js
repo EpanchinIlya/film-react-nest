9f5b6b5b092b3de502138d284773bbf4d622a323
Epanchin Ilya <epnchin.ilya@yandex.ru>
2025-05-04 20:32:46 +0500

    cc

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -29,7 +29,7 @@ module.exports = {
       //'pre-deploy': `scp C:/Users/Work/.ssh/id_rsa_pm2 C:/Project/film-react-nest/backend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend`,
       //'pre-deploy':'scp -i C:/Users/Work/.ssh/id_rsa_pm2 C:/Project/film-react-nest/backend/.env zoiberg@158.160.179.204:/home/zoiberg/film-react-nest_pm2_deploy/current/backend',
 
-      'post-deploy': `cd ${DEPLOY_PATH}/current/backend && npm install && npm run build && pm2 reload ecosystem.config.js --only api-service`,
+      'post-deploy': `cd /home/zoiberg/film-react-nest_pm2_deploy/current/backend && npm install && npm run build && pm2 reload ecosystem.config.js --only api-service`,
     },
   },
 };
