7c3bf2787294fdda5a09acca2d2b2c20dcfe06ae
Epanchin Ilya <epnchin.ilya@yandex.ru>
2025-05-10 18:42:48 +0500

    сделал тесты

--- backend/src/order/dto/order.dto.ts
+++ backend/src/order/dto/order.dto.ts
@@ -44,30 +44,3 @@ export interface OrderAnswer {
   total: number;
   items: (Omit<TicketDTO, 'day' | 'time'> & { id: string })[]; // добавляем поле id поверх TicketDTO
 }
-
-// {
-//   email: 'epanchin.ilya@yandex.ru',
-//   phone: '+7 (902) 269 49 55',
-//   tickets: [
-//     {
-//       film: '0e33c7f6-27a7-4aa0-8e61-65d7e5effecf',
-//       session: 'f2e429b0-685d-41f8-a8cd-1d8cb63b99ce',
-//       daytime: '2024-06-28T10:00:53+03:00',
-//       day: '28 июня',
-//       time: '12:00',
-//       row: 1,
-//       seat: 1,
-//       price: 350
-//     },
-//     {
-//       film: '0e33c7f6-27a7-4aa0-8e61-65d7e5effecf',
-//       session: 'f2e429b0-685d-41f8-a8cd-1d8cb63b99ce',
-//       daytime: '2024-06-28T10:00:53+03:00',
-//       day: '28 июня',
-//       time: '12:00',
-//       row: 1,
-//       seat: 2,
-//       price: 350
-//     }
-//   ]
-// }
--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -71,48 +71,4 @@ export class OrderService {
 
     return answer;
   }
-
-  // createOrder(body: OrderDTO): OrderAnswer {
-  //   const tickets = body.tickets; // буду получать весь body в сервис, а то вдруг понадобится email потом
-
-  //   tickets.forEach((ticket) => {
-  //     const schedule = this.mongoRepository.findById(ticket.film);
-  //     if (!schedule)
-  //       throw new NotFoundException(`Такого фильма нет в показе кинотеатра`); // не плохо бы перенести в контроллер
-
-  //     const session = schedule.find((item) => item.id === ticket.session);
-  //     if (!session) throw new NotFoundException(`На данное время сеансов нет`);
-
-  //     const taken = session.taken.find(
-  //       (item) => item === `${ticket.row}:${ticket.seat}`,
-  //     );
-  //     if (taken) throw new ConflictException('Выбранное место уже занято');
-  //     else {
-  //       this.mongoRepository.takeSeat(
-  //         ticket.film,
-  //         ticket.session,
-  //         ticket.row,
-  //         ticket.seat,
-  //       );
-  //     }
-  //   });
-
-  //   const fullTickets = tickets.map(
-  //     ({ film, session, daytime, row, seat, price }) => ({
-  //       film,
-  //       session,
-  //       daytime,
-  //       row,
-  //       seat,
-  //       price,
-  //       id: uuid(),
-  //     }),
-  //   );
-
-  //   const answer: OrderAnswer = {
-  //     total: tickets.length,
-  //     items: fullTickets,
-  //   };
-  //   return answer;
-  // }
 }
